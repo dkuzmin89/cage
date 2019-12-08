7c552fefb5aa9e36910c0c09211196da58ebda86
dkuzmin89 <dmitry.kuzmin@unil.ch>
2019-12-08 12:44:14 +0100

    !

--- CAGE_DETECTION_V2.1.py
+++ CAGE_DETECTION_V2.1.py
@@ -18,43 +18,50 @@ def img_read(input_dir, size):
     img = cv2.resize(common, (0, 0), common, 0.1, 0.1)
     return img
 
-def processing(img):
+
+def processing(img, c1=110, c2=400):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 110, 400)
-    ret, thresh = cv2.threshold(gray, 215, 255, cv2.THRESH_BINARY)
+    edges = cv2.Canny(gray, c1, c2)
+    _, thresh = cv2.threshold(gray, 215, 255, cv2.THRESH_BINARY)
     img_comb = edges + thresh
     return img_comb
 
-def morphologic (img_comb):
-    dilation = cv2.dilate(img_comb, np.ones((3, 3), np.uint8), iterations=3)
-    erosion = cv2.erode(dilation, np.ones((2, 2), np.uint8), iterations=2)
-    return erosion
 
-def mask (erosion):
-    mask = cv2.bitwise_not(erosion)
+def morphologic(img_comb, iter_d=3, iter_e=2):
+    dilation = cv2.dilate(img_comb, np.ones((3, 3), np.uint8), iterations=iter_d)
+    img_mask = cv2.erode(dilation, np.ones((2, 2), np.uint8), iterations=iter_e)
+    return img_mask
+
+
+def mask(img_mask):
+    mask = cv2.bitwise_not(img_mask)
     mask = cv2.resize(mask, size)
     mask = cv2.GaussianBlur(mask, (31, 31), 0)
     mask = cv2.GaussianBlur(mask, (21, 21), 0)
     return mask
 
-def applyMask(image, mask):
+
+def apply_mask(image, mask):
     image_size = (image.shape[1], image.shape[0])
     tmp_mask = cv2.resize(mask, image_size)
-    return cv2.bitwise_and(image,image, mask=tmp_mask)
+    return cv2.bitwise_and(image, image, mask=tmp_mask)
+
 
 if __name__ == '__main__':
 
     size = (1280, 760)
     input_dir = 'input'
+    folder_to_apply = "pics"
+    folder_with_result = "output"
 
     img = img_read(input_dir, size)
     img_comb = processing(img)
     erosion = morphologic(img_comb)
     mask = mask(erosion)
 
+    cv2.imwrite('output/MASK.png', mask)
 
-    folder_to_apply = "pics"
-    folder_with_result = "output"
+
     if not os.path.exists(folder_with_result):
         os.makedirs(folder_with_result)
 
@@ -62,10 +69,8 @@ if __name__ == '__main__':
         if pic.endswith('png'):
             pic_name = pic.rsplit('.', 1)[0]
             img = cv2.imread(os.path.join(folder_to_apply, pic))
-            result = applyMask(img, mask)
+            result = apply_mask(img, mask)
             cv2.imwrite(os.path.join(folder_with_result, pic_name + '_masked.png'), result)
 
-
-    # plt.imshow(mask, cmap='gray')
-    # plt.show()
-
+plt.imshow(mask, cmap='gray')
+plt.show()
